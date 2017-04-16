429b13674b85a18b145071c842ef704c233fb3a5
ChinLiang <qian15@gmail.com>
2017-04-16 17:17:48 -0400

    correct compilation error

--- src/main/java/com/chincent/App.java
+++ src/main/java/com/chincent/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.print
-        ln( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
